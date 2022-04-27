dfe57829fae7ca195c53ca2c4e93dc569ab38ea2
dengruchun <1107151332@qq.com>
2022-04-27 15:39:26 +0800

    队伍 对战

--- jx3/match.js
+++ jx3/match.js
@@ -2,25 +2,16 @@ import { createApp } from "https://unpkg.com/vue@next/dist/vue.esm-browser.prod.
 import Dexie from "https://unpkg.com/dexie@latest/dist/modern/dexie.min.mjs"
 import GithubCloud from "https://drc-git.github.io/scripts/github.js"
 
-
-const modifyTime = 'modifyTime'
-const uploadPath = 'jx3/match.json'
+const modifyTime = 'modifyTime';
+const uploadPath = 'jx3/match.json';
 const colorList = ["#9e9e9e", "#ff9800", "#ffc107", "#795548", "#ffeb3b", "#cddc39", "#4caf50", "#00bcd4", "#03a9f4", "#9c27b0"];
-
 const github = new GithubCloud({
     owner: "drc-git",
-    repo: "store",    
+    repo: "store",
     //简单混淆，避免 github Personal access tokens 被删除
     Authorization: 'N*O*W*s*5*0*I*x*u*2*I*j*i*4*J*z*A*o*G*0*X*t*l*v*l*a*b*1*u*M*o*0*3*L*R*W*_*p*h*g'.replace(/\*/g, '').split('').reverse().join(''),//end 2023-04-26
 });
-const matchServer = github.getJson(uploadPath);
-
 const db = new Dexie('match');
-db.version(2).stores({
-    leftList: 'ID',
-    rightList: 'ID',
-    log: 'ID'
-});
 
 function readJsonFile(file) {
     return new Promise((resolve, reject) => {
@@ -29,7 +20,6 @@ function readJsonFile(file) {
         reader.onload = function () {
             resolve(JSON.parse(reader.result))
         };
-
         reader.onerror = reject
     })
 }
@@ -37,12 +27,20 @@ function readJsonFile(file) {
 function randomInt(start, end) {
     return parseInt(Math.random() * (end - start + 1) + start, 10)
 }
+
+function getRandomRows(rows, n) {
+    const data = [];
+    for (let i = 0; i < n; i++) {
+        data.push(rows.splice(randomInt(0, rows.length - 1), 1)[0])
+    }
+    return data
+}
+
 const vApp = createApp({
     data() {
         return {
             leftList: [],
             rightList: [],
-            colorList,
             queueList: []
         }
     },
@@ -50,53 +48,58 @@ const vApp = createApp({
         otherList() {
             if (this.queueList.length) {
                 const set = new Set();
-                this.queueList.forEach(list => list.forEach(e => set.add(e.ID)));
-                return [...this.leftList.filter(e => e.ID.trim() && !set.has(e.ID)), ...this.rightList.filter(e => e.ID.trim() && !set.has(e.ID))]
+                this.queueList.forEach(list => list.forEach(t => t.queue.forEach(e => set.add(e.ID))));
+                return this.leftList.filter(e => e.ID.trim()).concat(this.rightList.filter(e => e.ID.trim())).filter(e => !set.has(e.ID))
             }
             return []
         }
     },
+    beforeCreate() {
+        db.version(2).stores({
+            leftList: 'ID',
+            rightList: 'ID',
+            log: 'ID'
+        })
+    },
     async mounted() {
-        const serverData = await matchServer
+        const serverData = await github.getJson(uploadPath);
         let leftList = await db.leftList.toArray();
         let rightList = await db.rightList.toArray();
-        const logTime = await db.log.get(modifyTime).then(e => e && e.time)
-
+        const logTime = await db.log.get(modifyTime).then(e => e && e.time);
         if (serverData && serverData[modifyTime] > (logTime ? logTime : 0)) {
             leftList = serverData.leftList;
-            rightList = serverData.rightList;
+            rightList = serverData.rightList
         }
-
         this.leftList = leftList;
         this.rightList = rightList;
-        this.reset();
+        this.reset()
     },
     methods: {
         match3v3() {
-            if (this.queueList.length) {
-                return
-            }
-            while (this.match(1, 2)) { }
+            this.matchT(1, 2)
         },
         match5v5() {
-            if (this.queueList.length) {
-                return
-            }
-            while (this.match(2, 3)) { }
+            this.matchT(2, 3)
         },
-        match(left, right) {
-            const leftMatchs = this.$refs.left.match(left);
-            if (!leftMatchs) {
-                return
+        matchT(left, right) {
+            this.reset();
+            const leftList = this.$refs.left.getMatchData();
+            const rightList = this.$refs.right.getMatchData();
+            const rowCount = Math.floor(Math.min(leftList.length / left, rightList.length / right));
+            const leftMatch = this.$refs.left.match(left, rowCount);
+            const rightMatch = this.$refs.right.match(right, rowCount);
+            const list = leftMatch.map((e, index) => ({
+                index,
+                queue: e.concat(rightMatch[index])
+            }));
+            const queueList = [];
+            while (list.length > 1) {
+                queueList.push(getRandomRows(list, 2))
             }
-            const rightMatchs = this.$refs.right.match(right);
-            if (!rightMatchs) {
-                return
+            if (list.length) {
+                queueList.push(list)
             }
-            this.$refs.left.matchLog(leftMatchs);
-            this.$refs.right.matchLog(rightMatchs);
-            this.queueList.push([...leftMatchs, ...rightMatchs]);
-            return true
+            this.queueList = queueList
         },
         onImport(e) {
             const target = e.target;
@@ -104,22 +107,22 @@ const vApp = createApp({
                 const data = JSON.parse(JSON.stringify(json));
                 if (json.leftList) {
                     this.leftList = json.leftList;
-                    this.onLeftChange(data.leftList);
+                    this.onLeftChange(data.leftList)
                 }
                 if (json.rightList) {
                     this.rightList = json.rightList;
-                    this.onRightChange(data.rightList);
+                    this.onRightChange(data.rightList)
                 }
             }).catch(() => {
-
+                alert("导入文件错误")
             });
-            target.value = "";
+            target.value = ""
         },
         onExport() {
             const file = new File([JSON.stringify({
                 leftList: this.$refs.left.getPlainData(),
                 rightList: this.$refs.right.getPlainData()
-            }, null, 2)], 'match.txt', { type: 'application/json' });
+            }, null, 2)], 'match.json', { type: 'application/json' });
             const url = URL.createObjectURL(file);
             const a = document.createElement('a');
             a.href = url;
@@ -139,7 +142,7 @@ const vApp = createApp({
         reset() {
             this.$refs.left.reset();
             this.$refs.right.reset();
-            this.queueList.length = 0;
+            this.queueList.length = 0
         },
         async onLeftChange(rows) {
             await db.leftList.clear();
@@ -179,8 +182,6 @@ vApp.component('v-list', {
     data() {
         return {
             rows: [],
-            alert: "",
-            matchColor: 0,
             matchMap: new Map()
         }
     },
@@ -194,7 +195,7 @@ vApp.component('v-list', {
         },
         setRows(list) {
             this.rows = list;
-            this.rows.push(this.gen());
+            this.rows.push(this.gen())
         },
         gen() {
             return { ID: "", Job: "" }
@@ -211,22 +212,17 @@ vApp.component('v-list', {
             this.$emit("change", this.getPlainData())
         },
         reset() {
-            this.matchColor = 0;
-            this.alert = "";
-            this.matchMap = new Map();
+            this.matchMap = new Map()
         },
-        match(n) {
+        match(n, max) {
             const list = this.getMatchData();
-            if (list.length - this.matchMap.size < n) {
-                this.alert = "剩余 ID 不足";
-                return
+            const rows = [];
+            for (let i = 0; i < max; i++) {
+                const matchList = getRandomRows(list, n);
+                matchList.forEach(e => this.matchMap.set(e.ID, i));
+                rows.push(matchList)
             }
-            const canMatch = list.filter(e => !this.matchMap.has(e.ID));
-            return this.getRandomData(canMatch, n);
-        },
-        matchLog(matchList) {
-            const colorIndex = this.matchColor++;
-            matchList.forEach(e => this.matchMap.set(e.ID, colorIndex));
+            return rows
         },
         getPlainData() {
             return this.rows.map(a => {
@@ -238,15 +234,6 @@ vApp.component('v-list', {
         },
         getMatchData() {
             return this.getPlainData().filter(a => a.ID)
-        },
-        getRandomData(rows, n) {
-            const copyRows = rows.slice();
-            const data = [];
-            for (let i = 0, max = rows.length - 1; i < n; i++) {
-                data.push(copyRows.splice(randomInt(0, max), 1)[0])
-                max--;
-            }
-            return data
         }
     },
     template: document.getElementById('template-list').innerHTML
